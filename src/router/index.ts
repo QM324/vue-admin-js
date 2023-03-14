68169fbbc3724d91804147c8b29f781a58f23698
kimer0324 <qianmei.wang@summerfarm.onaliyun.com>
2023-03-14 23:55:48 +0800

    fix: 登录路由

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+// 创建路由
+const router = createRouter({
+  // createWebHistory 后端协助配置转发（白屏，404）
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
